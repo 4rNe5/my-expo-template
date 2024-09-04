4c1eca0a4a2898f33a6a2aa917d60439c53f55fa
Park Jun Hyeon / 4rNe5 <me@4rne5.dev>
2024-09-04 09:32:32 +0900

    SETUP :: Expo Temp1ate Setup

---
+++ assets/fonts/fontLists.tsx
@@ -0,0 +1,12 @@
+export const fontLists = {
+    'Pretendard-Black': require('./Pretendard-Black.otf'),
+    'Pretendard-ExtraBold': require('./Pretendard-ExtraBold.otf'),
+    'Pretendard-Bold': require('./Pretendard-Bold.otf'),
+    'Pretendard-SemiBold': require('./Pretendard-SemiBold.otf'),
+    'Pretendard-Medium': require('./Pretendard-Medium.otf'),
+    'Pretendard-Regular': require('./Pretendard-Regular.otf'),
+    'Pretendard-Light': require('./Pretendard-Light.otf'),
+    'Pretendard-ExtraLight': require('./Pretendard-ExtraLight.otf'),
+    'Pretendard-Thin': require('./Pretendard-Thin.otf'),
+    'NeoDunggeunmoPro': require('./NeoDunggeunmoPro-Regular.ttf'),
+};
